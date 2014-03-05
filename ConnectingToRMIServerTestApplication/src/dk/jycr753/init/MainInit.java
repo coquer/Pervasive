9b2585bec52f37a86ff46e2a8e772e4b1c9854e9
jycr753 <self@jorgerodriguez.dk>
2014-03-05 13:28:16 +0100

    no important changes

---
+++ ConnectingToRMIServerTestApplication/src/dk/jycr753/init/MainInit.java
@@ -0,0 +1,31 @@
+package dk.jycr753.init;
+
+import dk.pervasive.jcaf.ContextEvent;
+import dk.pervasive.jcaf.EntityListener;
+import dk.pervasive.jcaf.impl.RemoteEntityListenerImpl;
+
+
+public class MainInit implements EntityListener {
+	
+	private RemoteEntityListenerImpl listener; 
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public MainInit(){
+		super();
+		
+	}
+	
+	@Override
+	public void contextChanged(ContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
