2fe88df0fac2943a5c5e69b3523ec6fa1fb0b410
JanettHolst <janettholst.apps@gmail.com>
2014-03-05 13:27:49 +0100

    Janett's Listener

---
+++ Pervasive4_task2_Listener/src/itu/janettholst/MainActivity.java
@@ -0,0 +1,15 @@
+package itu.janettholst;
+
+import java.rmi.RemoteException;
+
+
+public class MainActivity {
+	
+	
+	public static void main(String[] args) throws RemoteException{
+		
+		String location = "";
+		Listener listener = new Listener("rmi://10.25.252.253/myService");
+		
+	}
+}
