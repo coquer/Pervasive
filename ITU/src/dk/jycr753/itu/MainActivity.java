290509a260cfd1d4e7ab31470047dbf3620e62f5
jycr753 <self@jorgerodriguez.dk>
2014-03-06 13:25:05 +0100

    finally working....

--- ITU/src/dk/jycr753/itu/MainActivity.java
+++ ITU/src/dk/jycr753/itu/MainActivity.java
@@ -10,7 +10,6 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.location.Criteria;
@@ -20,9 +19,6 @@ public class MainActivity extends Activity implements LocationListener {
 	
 	private LocationManager locationManager;
 	private String provider;
-	private final Handler someHandler = new Handler();
-	private Location locationNetwork;
-	private Location locationGPS;
 	
 	
 	@Override
@@ -30,30 +26,30 @@ public class MainActivity extends Activity implements LocationListener {
 		super.onCreate(savedInstanceState);
 		System.out.println("Hello World 1");
 		setContentView(R.layout.activity_main);
-		new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                    	
-                        Thread.sleep(5000);
-                        someHandler.post(new Runnable() {
-                        	
-                            @Override
-                            public void run() {
-                                Criteria criteria = new Criteria();
-            					provider = locationManager.getBestProvider(criteria, false);
-                            	Location deviceLocation = locationManager.getLastKnownLocation(provider);
-                            	System.out.println("Location device :: " + deviceLocation);
-                            	
-                            }
-                        });
-                    } catch (Exception e) {
-                        System.out.println("Error in run() "+ e);
-                    }
-                }
-            }
-        }).start();
+//		new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                while (true) {
+//                    try {
+//                    	
+//                        Thread.sleep(5000);
+//                        someHandler.post(new Runnable() {
+//                        	
+//                            @Override
+//                            public void run() {
+//                                Criteria criteria = new Criteria();
+//            					provider = locationManager.getBestProvider(criteria, false);
+//                            	Location deviceLocation = locationManager.getLastKnownLocation(provider);
+//                            	System.out.println("Location device :: " + deviceLocation);
+//                            	
+//                            }
+//                        });
+//                    } catch (Exception e) {
+//                        System.out.println("Error in run() "+ e);
+//                    }
+//                }
+//            }
+//        }).start();
 		
 //		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 //		Criteria criteria = new Criteria();
@@ -72,7 +68,7 @@ public class MainActivity extends Activity implements LocationListener {
 		//boolean isNetworkEnable = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 		boolean isGPSEnable = true;
 		boolean isNetworkEnable = true;
-		System.out.println("Hello World 3 with this bools " + isGPSEnable + isNetworkEnable);
+		System.out.println("Hello World 3 with this bools " + isGPSEnable +" - "+ isNetworkEnable);
 		if(isGPSEnable == true && isNetworkEnable == true){
 			System.out.println("Hello World 4");
 			LocationResult locationResult = new LocationResult(){
@@ -80,12 +76,13 @@ public class MainActivity extends Activity implements LocationListener {
 			    public void gotLocation(Location location){
 			        TextView gotLocation = (TextView)findViewById(R.id.set_location_text_view);
 			        gotLocation.setText("Got it");
+			       
 			        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 					Criteria criteria = new Criteria();
 					provider = locationManager.getBestProvider(criteria, false);
 					Location androidLocation = locationManager.getLastKnownLocation(provider);
 					if(androidLocation != null){
-						//onLocationChanged(androidLocation);
+						onLocationChanged(location);
 					}else{
 						Toast.makeText(getApplicationContext(), "Error Bitch",
 								   Toast.LENGTH_LONG).show();
@@ -96,7 +93,7 @@ public class MainActivity extends Activity implements LocationListener {
 			};
 			GetLocation myLocation = new GetLocation();
 			myLocation.getLocation(this, locationResult);
-		}else {
+		} else {
 			System.out.println("Hello World 5");
 			showGPSDisabledAlertToUser();
 		}
@@ -107,7 +104,18 @@ public class MainActivity extends Activity implements LocationListener {
 	@Override
 	public void onResume(){
 		super.onResume();
-		locationManager.requestLocationUpdates(provider, 400, 1, this);
+		if(locationManager == null){
+			locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		}else{
+			locationManager.requestLocationUpdates(provider, 400, 1, this);
+		}
+		
+	}
+	
+	@Override
+	public void onPause(){
+		super.onPause();
+		locationManager.removeUpdates(this);
 	}
 
 	@Override
@@ -115,6 +123,10 @@ public class MainActivity extends Activity implements LocationListener {
 		// TODO Auto-generated method stub
 		Toast.makeText(getApplicationContext(), "Getting Data..",
 				   Toast.LENGTH_SHORT).show();
+		double lat = (double)(location.getLatitude());
+		double log = (double)(location.getLongitude());
+		TextView textViewAndroidLat = (TextView) findViewById(R.id.set_location_text_view);
+		textViewAndroidLat.setText(String.valueOf(lat) + " " + String.valueOf(log));
 		
 	}
 
