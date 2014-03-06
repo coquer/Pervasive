0ea9d88e6d3b860fc8ed3cfa91cc1c25e61bcd9d
jycr753 <self@jorgerodriguez.dk>
2014-03-06 13:01:52 +0100

    fucking crap
    Still crashing when trying to get the current location,

--- ITU/src/dk/jycr753/itu/GetLocation.java
+++ ITU/src/dk/jycr753/itu/GetLocation.java
@@ -1,7 +1,12 @@
 package dk.jycr753.itu;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -14,27 +19,43 @@ public class GetLocation {
 	    boolean gps_enabled=false;
 	    boolean network_enabled=false;
 
-	    public boolean getLocation(Context context, LocationResult result)
-	    {
-	        //I use LocationResult callback class to pass location value from MyLocation to user code.
+	    public boolean getLocation(Context context, LocationResult result){
 	        locationResult=result;
-	        if(lm==null)
-	            lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+	        if(lm==null){
+	        	lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+	        }
+	            
 
-	        //exceptions will be thrown if provider is not permitted.
-	        try{gps_enabled=lm.isProviderEnabled(LocationManager.GPS_PROVIDER);}catch(Exception ex){}
-	        try{network_enabled=lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);}catch(Exception ex){}
+	        try{
+	        	gps_enabled=lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+	        	
+	        }catch(Exception ex){
+	        	
+	        }
+	        try{
+	        	network_enabled=lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+	        	
+	        }catch(Exception ex){
+	        	
+	        }
 
-	        //don't start listeners if no provider is enabled
-	        if(!gps_enabled && !network_enabled)
-	            return false;
+	        if(!gps_enabled && !network_enabled){
+	        	
+	        	return false;
+	        }
+	            
 
-	        if(gps_enabled)
-	            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListenerGps);
-	        if(network_enabled)
-	            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListenerNetwork);
+	        if(gps_enabled){
+	        	lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListenerGps);
+	        }
+	            
+	        if(network_enabled){
+	        	lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListenerNetwork);
+	        }
+	            
 	        timer1=new Timer();
-	        timer1.schedule(new GetLastLocation(), 20000);
+	        timer1.schedule(new GetLastLocation(), 10000);
+	        
 	        return true;
 	    }
 
@@ -45,9 +66,15 @@ public class GetLocation {
 	            lm.removeUpdates(this);
 	            lm.removeUpdates(locationListenerNetwork);
 	        }
-	        public void onProviderDisabled(String provider) {}
-	        public void onProviderEnabled(String provider) {}
-	        public void onStatusChanged(String provider, int status, Bundle extras) {}
+	        public void onProviderDisabled(String provider) {
+	        	
+	        }
+	        public void onProviderEnabled(String provider) {
+	        	
+	        }
+	        public void onStatusChanged(String provider, int status, Bundle extras) {
+	        	
+	        }
 	    };
 
 	    LocationListener locationListenerNetwork = new LocationListener() {
@@ -57,9 +84,15 @@ public class GetLocation {
 	            lm.removeUpdates(this);
 	            lm.removeUpdates(locationListenerGps);
 	        }
-	        public void onProviderDisabled(String provider) {}
-	        public void onProviderEnabled(String provider) {}
-	        public void onStatusChanged(String provider, int status, Bundle extras) {}
+	        public void onProviderDisabled(String provider) {
+	        	
+	        }
+	        public void onProviderEnabled(String provider) {
+	        	
+	        }
+	        public void onStatusChanged(String provider, int status, Bundle extras) {
+	        	
+	        }
 	    };
 
 	    class GetLastLocation extends TimerTask {
@@ -76,10 +109,11 @@ public class GetLocation {
 
 	             //if there are both values use the latest one
 	             if(gps_loc!=null && net_loc!=null){
-	                 if(gps_loc.getTime()>net_loc.getTime())
+	                 if(gps_loc.getTime()>net_loc.getTime()){
 	                     locationResult.gotLocation(gps_loc);
-	                 else
+	                 } else{
 	                     locationResult.gotLocation(net_loc);
+	                 }
 	                 return;
 	             }
 
@@ -98,4 +132,5 @@ public class GetLocation {
 	    public static abstract class LocationResult{
 	        public abstract void gotLocation(Location location);
 	    }
+
 }
--- ITU/src/dk/jycr753/itu/MainActivity.java
+++ ITU/src/dk/jycr753/itu/MainActivity.java
@@ -2,27 +2,159 @@ package dk.jycr753.itu;
 
 import dk.jycr753.itu.GetLocation.LocationResult;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.location.Criteria;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements LocationListener {
 	
+	private LocationManager locationManager;
+	private String provider;
+	private final Handler someHandler = new Handler();
+	private Location locationNetwork;
+	private Location locationGPS;
 	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		System.out.println("Hello World 1");
 		setContentView(R.layout.activity_main);
+		new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                    	
+                        Thread.sleep(5000);
+                        someHandler.post(new Runnable() {
+                        	
+                            @Override
+                            public void run() {
+                                Criteria criteria = new Criteria();
+            					provider = locationManager.getBestProvider(criteria, false);
+                            	Location deviceLocation = locationManager.getLastKnownLocation(provider);
+                            	System.out.println("Location device :: " + deviceLocation);
+                            	
+                            }
+                        });
+                    } catch (Exception e) {
+                        System.out.println("Error in run() "+ e);
+                    }
+                }
+            }
+        }).start();
 		
-		LocationResult locationResult = new LocationResult(){
-		    @Override
-		    public void gotLocation(Location location){
-		        TextView gotLocation = (TextView)findViewById(R.id.set_location_text_view);
-		        gotLocation.setText("Got it");
-		    }
-		};
-		GetLocation myLocation = new GetLocation();
-		myLocation.getLocation(this, locationResult);
+//		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//		Criteria criteria = new Criteria();
+//		provider = locationManager.getBestProvider(criteria, false);
+//		android.location.Location androidLocation = locationManager.getLastKnownLocation(provider);
+//		
+//		if(androidLocation != null){
+//			System.out.println("Provider -- " + provider);
+//			onLocationChanged(androidLocation);
+//		}else{
+//			System.out.println("No location affable");
+//		}
+//		
+		System.out.println("Hello World 2");
+		//boolean isGPSEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		//boolean isNetworkEnable = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+		boolean isGPSEnable = true;
+		boolean isNetworkEnable = true;
+		System.out.println("Hello World 3 with this bools " + isGPSEnable + isNetworkEnable);
+		if(isGPSEnable == true && isNetworkEnable == true){
+			System.out.println("Hello World 4");
+			LocationResult locationResult = new LocationResult(){
+			    @Override
+			    public void gotLocation(Location location){
+			        TextView gotLocation = (TextView)findViewById(R.id.set_location_text_view);
+			        gotLocation.setText("Got it");
+			        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+					Criteria criteria = new Criteria();
+					provider = locationManager.getBestProvider(criteria, false);
+					Location androidLocation = locationManager.getLastKnownLocation(provider);
+					if(androidLocation != null){
+						//onLocationChanged(androidLocation);
+					}else{
+						Toast.makeText(getApplicationContext(), "Error Bitch",
+								   Toast.LENGTH_LONG).show();
+					}
+					
+			        
+			    }
+			};
+			GetLocation myLocation = new GetLocation();
+			myLocation.getLocation(this, locationResult);
+		}else {
+			System.out.println("Hello World 5");
+			showGPSDisabledAlertToUser();
+		}
+		
+		
 	}
+	
+	@Override
+	public void onResume(){
+		super.onResume();
+		locationManager.requestLocationUpdates(provider, 400, 1, this);
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		Toast.makeText(getApplicationContext(), "Getting Data..",
+				   Toast.LENGTH_SHORT).show();
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	 private void showGPSDisabledAlertToUser(){
+	        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+	        alertDialogBuilder.setMessage("GPS is disabled in your device. Would you like to enable it?")
+	        .setCancelable(false)
+	        .setPositiveButton("Goto Settings Page To Enable GPS",
+	                new DialogInterface.OnClickListener(){
+	            public void onClick(DialogInterface dialog, int id){
+	                Intent callGPSSettingIntent = new Intent(
+	                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+	                startActivity(callGPSSettingIntent);
+	            }
+	        });
+	        alertDialogBuilder.setNegativeButton("Cancel",
+	                new DialogInterface.OnClickListener(){
+	            public void onClick(DialogInterface dialog, int id){
+	                dialog.cancel();
+	            }
+	        });
+	        AlertDialog alert = alertDialogBuilder.create();
+	        alert.show();
+	    }
 }
