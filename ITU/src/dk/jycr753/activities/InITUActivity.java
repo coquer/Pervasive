071e26a5173577bd9fcf3ee3c31a05eb0df454e6
jycr753 <self@jorgerodriguez.dk>
2014-03-18 09:24:08 +0100

    android

--- ITU/src/dk/jycr753/activities/InITUActivity.java
+++ ITU/src/dk/jycr753/activities/InITUActivity.java
@@ -1,5 +1,6 @@
 package dk.jycr753.activities;
 
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.UUID;
 
@@ -16,25 +17,35 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import dk.jycr753.bluetooth.AcceptThread;
+import dk.jycr753.bluetooth.ConnectThread;
 import dk.jycr753.bluetooth.ConnectionThread;
-import dk.jycr753.bluetooth.DeviceListActivity;
 import dk.jycr753.bluetooth.GetDeviceBluetoothInfo;
 import dk.jycr753.itu.R;
 import dk.jycr753.network.JsonParser;
 import dk.jycr753.strings.BlipURi;
 
 public class InITUActivity extends Activity {
-	
+
 	private static final String TAG = "InITUActivity";
 	JSONArray locationsArray = null;
+	private static final int REQUEST_ENABLE_BT = 0;
+	private static final int SELECT_SERVER = 1;
 	public static final int DATA_RECEIVED = 3;
 	public static final int SOCKET_CONNECTED = 4;
-	public static final UUID APP_UUID = UUID.fromString("aeb9f938-a1a3-4947-ace2-9ebd0c67adf1");
-	
+	public static final UUID APP_UUID = UUID
+			.fromString("aeb9f938-a1a3-4947-ace2-9ebd0c67adf1");
+
+	private boolean mServerMode;
+	private String data;
+
 	private BluetoothAdapter mBluetoothAdapter = null;
 	private ConnectionThread mBluetoothConnection = null;
 
@@ -63,11 +74,12 @@ public class InITUActivity extends Activity {
 		// **************************************
 
 		if (GetDeviceBluetoothInfo.isBluetoothEnable()) {
-			
-			
-			
+			startAsServer();
+			mServerMode = true;
+
 		} else {
 			readytext.setText("Enable Bluetooth");
+			finish();
 			// call blue tooth activity
 		}
 
@@ -100,32 +112,64 @@ public class InITUActivity extends Activity {
 	// **************************************
 	// ****** RUN BLUETOOTH SCANNER
 	// **************************************
-//	public class ConnectToBluetooth extends AsyncTask<String, Void, String> { 
-//
-//		@Override
-//		protected void onPreExecute() {
-//			super.onPreExecute();
-//			
-//		}
-//
-//		@Override
-//		protected String doInBackground(String... params) {
-//			System.out.println("doInBackground");
-//			boolean isConnected = 
-//			
-//			return "J";
-//			
-//		}
-//
-//		@Override
-//		protected void onPostExecute(String result) {
-//
-//			TextView readytext = (TextView) findViewById(R.id.itu_text_view_title);
-//			readytext.setText("result " + result);
-//			System.out.println("result - " + result);
-//		}
-//
-//	}
+	
+
+	public Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case SOCKET_CONNECTED: {
+				mBluetoothConnection = (ConnectionThread) msg.obj;
+				if (!mServerMode)
+					mBluetoothConnection.write("this is a message".getBytes());
+				break;
+			}
+			case DATA_RECEIVED: {
+				data = (String) msg.obj;
+				Log.i(TAG, "Message - " + data);
+				if (mServerMode)
+					mBluetoothConnection.write(data.getBytes());
+			}
+			default:
+				break;
+			}
+		}
+	};
+
+	private void selectServer() {
+
+		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter
+				.getBondedDevices();
+		ArrayList<String> pairedDeviceStrings = new ArrayList<String>();
+		if (pairedDevices.size() > 0) {
+			for (BluetoothDevice device : pairedDevices) {
+				pairedDeviceStrings.add(device.getName() + "\n"
+						+ device.getAddress());
+			}
+		}
+		Intent showDevicesIntent = new Intent(this, ShowDevices.class);
+		showDevicesIntent.putStringArrayListExtra("devices",
+				pairedDeviceStrings);
+		startActivityForResult(showDevicesIntent, SELECT_SERVER);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_OK) {
+		} else if (requestCode == SELECT_SERVER && resultCode == RESULT_OK) {
+			BluetoothDevice device = data
+					.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+			connectToBluetoothServer(device.getAddress());
+		}
+	}
+	
+	private void connectToBluetoothServer(String id) {
+		new ConnectThread(id, mHandler).start();
+	}
+	
+	private void startAsServer() {
+		new AcceptThread(mHandler).start();
+	}
 
 	@Override
 	protected void onResume() {
